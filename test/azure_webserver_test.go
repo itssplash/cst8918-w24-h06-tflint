9bc5ec404d24b1b14996912583c9bedd6c7c568b
Abhinav Abhinav <abhinaavv007@gmail.com>
2025-02-14 03:17:43 -0500

    Added TFLint and fixed reported issues

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "454cef6e-c0dd-444f-9766-2fcc4e902f67"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"labelPrefix": "abhi0012",
 		},
 	}
 
